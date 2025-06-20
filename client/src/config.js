8374df5c1feb4134b29fbd71a32b2a6e5fb28cb1
AYUSH RAWAT <rawatayush004@gmail.com>
2025-06-20 13:16:42 +0530

    deployment

--- client/src/config.js
+++ client/src/config.js
@@ -2,7 +2,7 @@
 const config = {
   // Since we're serving the client from the same server, we can use relative URLs
   // or detect the current hostname automatically
-  SERVER_URL: window.location.origin.replace(/:\d+$/, ':3001') || "http://localhost:3001",
+  SERVER_URL: "https://virtual-classrom.onrender.com",
   
   // For development, you can still override this
   // SERVER_URL: process.env.REACT_APP_SERVER_URL || "http://localhost:3001",
